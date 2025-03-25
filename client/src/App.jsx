7d1b966bc3c10eac0236f4f8b85ebfadc4445296
Jembere <B.Jembere@cgiar.org>
2025-03-25 10:10:36 +0300

    fixing some errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import BioCourses from "./pages/Department/Biology/Courses/MyCourses";
 import MathsCourses from "./pages/Department/Maths/Courses/MyCourses";
 import Materials from "./components/Materials";
 import Semester from "./components/common/Semester";
-import FileList from './pages/Department/Physics/Courses/phynotes'
+import FileList from './pages/Department/Physics/Courses/phynotes';
 
 function App() {
   return (
